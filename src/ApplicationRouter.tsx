ea808eac4cf43cc9026d0b1b7d999c299c928450
Dpascuttini <dominik.pascuttini1@gmail.com>
2020-10-29 16:01:34 +0100

    dog first react stuff

--- src/ApplicationRouter.tsx
+++ src/ApplicationRouter.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Test } from './pages/Test';
 export const ApplicationRouter = () => {
   return (
     <div className='flex flex-col h-screen justify-between mb-auto'>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/test' component={Test} />
       </Switch>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { InstagramLogo } from '../assets';
+import { Card, Item } from '../components/Card';
 
-interface Props {}
+const item: Item = {
+  slug: 'test',
+  img: InstagramLogo,
+  name: 'name',
+  description: 'loremdasmdasdasdasdas',
+};
+export const Home = () => {
+  const [counter, setcounter] = useState(0);
+  return (
+    <div className='container mx-auto pt-5'>
+      <h1 className='text-2xl'>Home</h1>
+      <div className='flex space-x-2 items-center'>
+        <button
+          className='border border-gray-300 rounded-md p-3'
+          onClick={() => {
+            setcounter(counter + 1);
+          }}
+        >
+          up
+        </button>
+        <div className='text-center'>{counter}</div>
+        <button
+          className='border border-gray-300 rounded-md p-3'
+          onClick={() => {
+            setcounter(counter - 1);
+          }}
+        >
+          down
+        </button>
+      </div>
 
-export const Home = (props: Props) => {
-  return <div>Home</div>;
+      <Card item={item} />
+    </div>
+  );
 };
